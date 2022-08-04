2ab6ab092154378621c4bd2aea4992d6433a2939
Jr Bytes <itamazor@gmail.com>
2022-08-03 22:12:40 -0300

    adicionado layout de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      Helloworld
-      <button>teste</button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
